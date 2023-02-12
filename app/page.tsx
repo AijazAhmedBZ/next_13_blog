d1945ce8989b4c316f63c74230d5986be130cdba
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-02-12 14:50:12 +0500

    1st cimmit and push

---
+++ app/page.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+import Link from 'next/link'
+import { posts } from '@/data/post'
+import { type } from 'os'
+
+const inter = Inter({ subsets: ['latin'] })
+
+type Post = {
+  id: string,
+  title: string,
+  date: string
+}
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}><h1>Home</h1></div>
+      <div>
+        {posts.map(({id, title, date}: Post) => (
+          <>
+          <Link href={`/posts/${id}`}>{title}</Link>
+          <br />
+          <p>{date}</p>
+          <br />
+          </>
+        ))}
+      </div>
+      <Link href="/posts">Post</Link>
+      <Link href="/account">Account</Link>
+    </main>
+  )
+}
